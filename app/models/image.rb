102e47153ad968397ddb8437f33b22c913a96708
otukutun <orehaorz@gmail.com>
2019-02-28 08:42:02 +0900

    imageのvalidationつけた.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,26 @@
 class Image < ApplicationRecord
   has_one_attached :content
+  validate :validate_content
 
   def thumbnail
     content.variant(resize: '600x600').processed
+  end
+
+  def validate_content
+    unless content.attached?
+      errors.add(:content, 'File is empty')
+      return
+    end
+    if content.blob.byte_size > 10.megabytes
+      content.purge
+      errors.add(:content, 'File size is too large')
+    elsif !valid_image_type?
+      content.purge
+      errors.add(:content, 'File type is not valid')
+    end
+  end
+
+  def valid_image_type?
+    %w[image/jpg image/jpeg image/gif image/png].include?(content.blob.content_type)
   end
 end
