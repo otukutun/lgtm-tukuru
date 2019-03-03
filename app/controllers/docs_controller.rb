0db9461936c25c427c71c3c0fef500f37a699e58
otukutun <orehaorz@gmail.com>
2019-03-03 18:24:53 +0900

    Formated API doc.

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,6 +1,17 @@
 class DocsController < ApplicationController
   def api
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions = {})
+    options = {
+      autolink: true,
+      space_after_headers: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      tables: true,
+      hard_wrap: true,
+      xhtml: true,
+      lax_html_blocks: true,
+      strikethrough: true
+    }
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
     doc = File.read(Rails.root.join("docs", "api.md"))
     @api_doc = markdown.render(doc)
   end
