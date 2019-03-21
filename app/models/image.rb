2314c91b2daa53b921d4b345816c022d764eab31
otukutun <orehaorz@gmail.com>
2019-03-21 15:14:51 +0900

    Add file present validation.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,10 @@
 class Image < ApplicationRecord
   mount_uploader :file, LgtmUploader
 
-  validates :file, file_size: { less_than: 10.gigabytes }
+  validates :file,
+    presence: true,
+    file_size: { less_than: 10.megabytes },
+    file_content_type: { allow: ['image/jpeg', 'image/gif', 'image/png'] }
   before_create {
     self.slug =  SecureRandom.hex(20)
   }
